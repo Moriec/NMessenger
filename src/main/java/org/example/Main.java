ef00ae1f3ebfbdd653179a607cb7cd52b4d5adb5
Михаил <mikegib@mail.ru>
2024-12-07 10:45:53 +0300

    Гибалин исправил Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,9 @@ import org.example.services.*;
 public class Main {
     public static void main(String[] args) {
         UserService userService = new UserService();
+        userService.create4Users();
         MessageService messageService = new MessageService();
+        messageService.create4Message(userService.getUsers());
         userService.printUserInfo(userService.getUsers()[0]);
     }
 }
