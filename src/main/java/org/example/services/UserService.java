283ccce88d36c237a87d20eebe065a04b31bba0a
Fazam-coder <amirrf06@gmail.com>
2024-12-07 10:08:57 +0300

    Фазульзянов сервисы

--- src/main/java/org/example/services/UserService.java
+++ src/main/java/org/example/services/UserService.java
@@ -1,4 +1,12 @@
 package org.example.services;
 
+import org.example.model.User;
+
 public class UserService {
+    private User[] users = new User[4];
+
+    public void create4Users() {
+        users[0] = new User("Mike", "Михаил", "88005553535", "123");
+    }
+
 }
