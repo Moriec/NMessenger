98825e21eeed137ae4fe72bbcb0284f3f4715c56
Vinogradov Dmitriy <vinogradovdima123456789@gmail.com>
2024-12-07 09:48:51 +0300

    Vinogradov, add getters and setters in Message.class

--- src/main/java/org/example/model/Message.java
+++ src/main/java/org/example/model/Message.java
@@ -1,4 +1,50 @@
 package org.example.model;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Message {
+    private String sender, recipient, text, dateTime;
+
+    public Message(){
+        LocalDateTime currentDateTime = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String formattedDateTime = currentDateTime.format(formatter);
+        dateTime = formattedDateTime;
+    }
+
+    
+    //Getters
+    public String getDateTime() {
+        return dateTime;
+    }
+
+    public String getRecipient() {
+        return recipient;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    //Setters
+    public void setDateTime(String dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public void setRecipient(String recipient) {
+        this.recipient = recipient;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
 }
