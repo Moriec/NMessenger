8270e6d894d64a5b30bb4e7e8047eabe13912c10
Михаил <mikegib@mail.ru>
2024-12-07 10:41:56 +0300

    Гибалин вывод сведений в Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,11 @@
 package org.example;
 import org.example.model.*;
+import org.example.services.*;
 
 public class Main {
     public static void main(String[] args) {
-
-    }
-
-    public static void newMessage() {
-        System.out.println("ygdrygfyue");
+        UserService userService = new UserService();
+        MessageService messageService = new MessageService();
+        userService.printUserInfo(userService.getUsers()[0]);
     }
 }
