7b178c48d4bfe53532c765d627df7b2da12a5992
Fazam-coder <amirrf06@gmail.com>
2024-12-07 10:22:00 +0300

    Фазульзянов создал users

--- src/main/java/org/example/services/UserService.java
+++ src/main/java/org/example/services/UserService.java
@@ -7,6 +7,13 @@ public class UserService {
 
     public void create4Users() {
         users[0] = new User("Mike", "Михаил", "88005553535", "123");
+        users[1] = new User();
+        users[1].setNickname("Fazam");
+        users[1].setUsername("Амир");
+        users[1].setPhone("+76354634635");
+        users[1].setPassword("5236742");
+        users[2] = new User("ДИМООООООН", "Дмитрий", "+73247524567", "6234");
+        users[3] = new User("Олег", "Антон", "+76384667348", "87364");
     }
 
 }
